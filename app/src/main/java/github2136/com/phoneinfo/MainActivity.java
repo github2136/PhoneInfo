28b0132e582d3199a70c3e4e4623a7b1ec4bd7bf
github2136 <github2136@163.com>
2017-10-26 17:03:24 +0800

    添加信息

--- app/src/main/java/github2136/com/phoneinfo/MainActivity.java
+++ app/src/main/java/github2136/com/phoneinfo/MainActivity.java
@@ -1,7 +1,12 @@
 package github2136.com.phoneinfo;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+
+import java.nio.channels.ScatteringByteChannel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,156 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvInfo = (TextView) findViewById(R.id.tv_info);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        StringBuilder sb = new StringBuilder();
+        //屏幕信息
+        sb.append("屏幕信息");
+        sb.append("\n");
+        sb.append("宽度(widthPixels)：");
+        sb.append(dm.widthPixels);
+        sb.append("\n");
+        sb.append("高度(heightPixels)：");
+        sb.append(dm.heightPixels);
+        sb.append("\n");
+        sb.append("逻辑密度(density)：");
+        sb.append(dm.density);
+        sb.append("\n");
+        sb.append("密度(densityDpi)：");
+        sb.append(dm.densityDpi);
+        sb.append("\n");
+        sb.append("缩放密度(scaledDensity)：");
+        sb.append(dm.scaledDensity);
+        sb.append("\n");
+        sb.append("X轴密度(xdpi)：");
+        sb.append(dm.xdpi);
+        sb.append("\n");
+        sb.append("Y轴密度(ydpi)：");
+        sb.append(dm.ydpi);
+        sb.append("\n");
+        sb.append("资源文件夹：");
+        sb.append(getResources().getString(R.string.density_str));
+        sb.append("\n");
+        //build
+        sb.append("\n\n");
+        sb.append("底板信息(BOARD)：\n");
+        sb.append(Build.BOARD);
+        sb.append("\n\n");
+        sb.append("系统引导版本号(BOOTLOADER)：\n");
+        sb.append(Build.BOOTLOADER);
+        sb.append("\n\n");
+        sb.append("品牌名(BRAND)：\n");
+        sb.append(Build.BRAND);
+        sb.append("\n\n");
+        sb.append("设备型号(DEVICE)：\n");
+        sb.append(Build.DEVICE);
+        sb.append("\n\n");
+        sb.append("显示ID(DISPLAY)：\n");
+        sb.append(Build.DISPLAY);
+        sb.append("\n\n");
+        sb.append("唯一标示字符(FINGERPRINT)：\n");
+        sb.append(Build.FINGERPRINT);
+        sb.append("\n\n");
+        sb.append("硬件名(HARDWARE)：\n");
+        sb.append(Build.HARDWARE);
+        sb.append("\n\n");
+        sb.append("(HOST)：\n");
+        sb.append(Build.HOST);
+        sb.append("\n\n");
+        sb.append("id(ID)：\n");
+        sb.append(Build.ID);
+        sb.append("\n\n");
+        sb.append("制造商(MANUFACTURER)：\n");
+        sb.append(Build.MANUFACTURER);
+        sb.append("\n\n");
+        sb.append("设备名(MODEL)：\n");
+        sb.append(Build.MODEL);
+        sb.append("\n\n");
+        sb.append("产品名名(PRODUCT)：\n");
+        sb.append(Build.PRODUCT);
+        sb.append("\n\n");
+        sb.append("硬件编号(SERIAL)：\n");
+        sb.append(Build.SERIAL);
+        sb.append("\n\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            for (int i = 0, len = Build.SUPPORTED_32_BIT_ABIS.length; i < len; i++) {
+                String supported32BitAbi = Build.SUPPORTED_32_BIT_ABIS[i];
+                sb.append("CPU32位ABI(SUPPORTED_32_BIT_ABIS)：\n");
+                sb.append(supported32BitAbi);
+                sb.append("\n\n");
+            }
+
+            for (int i = 0, len = Build.SUPPORTED_64_BIT_ABIS.length; i < len; i++) {
+                String supported64BitAbi = Build.SUPPORTED_64_BIT_ABIS[i];
+                sb.append("CPU64位ABI(SUPPORTED_64_BIT_ABIS)：\n");
+                sb.append(supported64BitAbi);
+                sb.append("\n\n");
+            }
+
+            for (int i = 0, len = Build.SUPPORTED_ABIS.length; i < len; i++) {
+                String supportedBitAbi = Build.SUPPORTED_ABIS[i];
+                sb.append("CPU所有ABI(SUPPORTED_ABIS)：\n");
+                sb.append(supportedBitAbi);
+                sb.append("\n\n");
+            }
+        }
+        sb.append("构建类型(TAGS)：\n");
+        sb.append(Build.TAGS);
+        sb.append("\n\n");
+        sb.append("(TIME)：\n");
+        sb.append(Build.TIME);
+        sb.append("\n\n");
+        sb.append("构建类型(TYPE)：\n");
+        sb.append(Build.TYPE);
+        sb.append("\n\n");
+        sb.append("构建的未知属性值(UNKNOWN)：\n");
+        sb.append(Build.UNKNOWN);
+        sb.append("\n\n");
+        sb.append("(USER)：\n");
+        sb.append(Build.USER);
+        sb.append("\n\n");
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            sb.append("CPU_ABI(CPU_ABI)：\n");
+            sb.append(Build.CPU_ABI);
+            sb.append("\n\n");
+            sb.append("CPU_ABI2(CPU_ABI2)：\n");
+            sb.append(Build.CPU_ABI2);
+            sb.append("\n\n");
+        }
+        sb.append("无线电固件版本(getRadioVersion)：\n");
+        sb.append(Build.getRadioVersion());
+        sb.append("\n\n");
+        //
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            sb.append("基础系统版本(BASE_OS)：\n");
+            sb.append(Build.VERSION.BASE_OS);
+            sb.append("\n\n");
+        }
+        sb.append("开发代号(CODENAME)：\n");
+        sb.append(Build.VERSION.CODENAME);
+        sb.append("\n\n");
+        sb.append("(INCREMENTAL)：\n");
+        sb.append(Build.VERSION.INCREMENTAL);
+        sb.append("\n\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            sb.append("预览SDK版本号(PREVIEW_SDK_INT)：\n");
+            sb.append(Build.VERSION.PREVIEW_SDK_INT);
+            sb.append("\n\n");
+        }
+        sb.append("用户可见版本号(RELEASE)：\n");
+        sb.append(Build.VERSION.RELEASE);
+        sb.append("\n\n");
+        sb.append("SDK版本(SDK_INT)：\n");
+        sb.append(Build.VERSION.SDK_INT);
+        sb.append("\n\n");
+        sb.append("SDK版本(SDK)：\n");
+        sb.append(Build.VERSION.SDK);
+        sb.append("\n\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            sb.append("用户可见的安全补丁级别(SECURITY_PATCH)：\n");
+            sb.append(Build.VERSION.SECURITY_PATCH);
+            sb.append("\n\n");
+        }
+        tvInfo.append(sb.toString());
     }
 }
